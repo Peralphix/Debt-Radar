7d3f2dde4dc2b9036bd87dff40d9b958e9ec010b
dmitry.rom <dmitry.rom@cadolabs.io>
2026-04-26 17:55:04 +0300

    Add token check

--- app/controllers/public_api_controller.rb
+++ app/controllers/public_api_controller.rb
@@ -2,6 +2,7 @@
 
 class PublicApiController < ApplicationController
   skip_before_action :verify_authenticity_token, raise: false
+  before_action :authenticate_internal_api!
 
   def check_user
     # binding.irb
@@ -16,5 +17,14 @@ class PublicApiController < ApplicationController
   def debtor_params
     params.require(:inn)
     params.permit(:inn, :full_name, :vin, :sts_number)
+  end
+
+  def authenticate_internal_api!
+    token = request.headers["Authorization"].to_s.delete_prefix("Bearer ")
+
+    head :unauthorized unless ActiveSupport::SecurityUtils.secure_compare(
+      token,
+      ENV.fetch("INTERNAL_API_TOKEN")
+    )
   end
 end
