119f9fa455606351a64bdc21538c701c6a659ca1
dmitry.rom <dmitry.rom@cadolabs.io>
2026-04-26 17:29:12 +0300

    Fix config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.enable_reloading = false
   config.eager_load = true
   config.consider_all_requests_local = false
-  config.force_ssl = true
+  config.force_ssl = false
   config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info').to_sym
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
