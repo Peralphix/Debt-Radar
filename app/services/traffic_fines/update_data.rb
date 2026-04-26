4ca3c2021abba7b9c80771261d7ccd9deb941b13
dmitry.rom <dmitry.rom@cadolabs.io>
2026-04-26 18:22:15 +0300

    dev

--- app/services/traffic_fines/update_data.rb
+++ app/services/traffic_fines/update_data.rb
@@ -8,12 +8,12 @@ class TrafficFines::UpdateData
   def call
     return if @vehicle.traffic_fines_updated_at && @vehicle.traffic_fines_updated_at > 1.day.ago
 
-    result = TrafficFines::Search.new(@vehicle.sts_number).call
+    result = TrafficFines::Search.new(@vehicle.sts_number).call.to_h
     return if result.blank?
 
     @vehicle.update!(
-      traffic_fines_parsed_data: result[:parsed_data].to_h,
-      traffic_fines_raw_data: result[:raw_data].to_h,
+      traffic_fines_parsed_data: result[:parsed_data],
+      traffic_fines_raw_data: result[:raw_data],
       traffic_fines_updated_at: Time.current
     )
   rescue # как-нибудь изящнее потом сделать
